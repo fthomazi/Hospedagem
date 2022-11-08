387b9d16cec0bd38dd14c62fab24e692f5b49cff
Perkles <feer_thomazi@hotmail.com>
2022-11-07 23:05:58 -0300

    Atualização codigo

---
+++ Models/Reserva.cs
@@ -0,0 +1,68 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            try
+            {
+                if (Suite.Capacidade >= hospedes.Count)
+                {
+                    Hospedes = hospedes;
+                }
+                else
+                {
+                    // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                    // *IMPLEMENTE AQUI*
+                    Console.WriteLine($"Não há capacidade para um novo hóspede!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex}");
+            }
+
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            int quantidade = Hospedes.Count;
+            return quantidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                valor = (valor-((valor * 10)/100) );
+            }
+
+            return valor;
+        }
+    }
+}
